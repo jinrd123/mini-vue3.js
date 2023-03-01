fac8255d6c81aabab49e585b298e54ff8aa7196b
jinrongda <2368741842@qq.com>
2023-03-01 20:43:13 +0800

    fix(main): [reativity][effect]->修改依赖为Set

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,6 +1,7 @@
+import { createDep, Dep } from './dep'
 // targetMap为一个WeakMap，相当于维护的整个vue应用的响应式对象到对应的副作用函数的映射关系
-// targetMap的key值存放所有的响应式对象，对应的value为一个Map<KeyToDepMap>，其key一般情况下可以理解为响应式对象的属性名，value即为对回调函数fn的包装类实例
-type KeyToDepMap = Map<any, ReactiveEffect>
+// targetMap:( 被Reactive方法包装的对象 --> Map:( 对象属性名 --> Set<effect> ) )
+type KeyToDepMap = Map<any, Dep>
 const targetMap = new WeakMap<any, KeyToDepMap>()
 
 // effect函数的核心逻辑就是：（借助包装类）1.更新此模块的activeEffect 2. 调用fn
@@ -13,7 +14,7 @@ export function effect<T = any>(fn: () => T) {
 // 记录上一个effect调用的对应包装类
 export let activeEffect: ReactiveEffect | undefined
 
-// 对调用effect函数传入的回调函数的包装类
+// effect(fn)中fn的包装类
 export class ReactiveEffect<T = any> {
   constructor(public fn: () => T) {}
 
@@ -30,31 +31,50 @@ export function track(target: object, key: unknown) {
     return
   }
 
-  // depMap即为响应式对象的属性与依赖的对应
-  let depMap = targetMap.get(target)
-  if (!depMap) {
-    targetMap.set(target, (depMap = new Map()))
+  // depsMap即为Map:( 对象属性名 --> Set<effect> )
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
   }
 
-  depMap.set(key, activeEffect)
+  let dep = depsMap.get(key)
+  if (!dep) {
+    depsMap.set(key, (dep = createDep()))
+  }
 
-  console.log(targetMap)
+  trackEffects(dep)
+}
+
+export function trackEffects(dep: Dep) {
+  dep.add(activeEffect!)
 }
 
 // 触发依赖
-// 核心逻辑即执行targetMap[target][key].fn(存在的话)
+// 核心逻辑即执行所有targetMap[target][key].fn （targetMap[target][key]是一个Set集合）
 export function trigger(target: object, key: unknown) {
   const depsMap = targetMap.get(target)
-
   if (!depsMap) {
     return
   }
 
-  const effect = depsMap.get(key) as ReactiveEffect
+  const dep: Dep | undefined = depsMap.get(key)
 
-  if (!effect) {
+  if (!dep) {
     return
   }
 
-  effect.fn()
+  triggerEffects(dep)
+}
+
+// 执行Set<effect>集合中所有effect相关的逻辑
+export function triggerEffects(dep: Dep) {
+  const effects = Array.isArray(dep) ? dep : [...dep]
+
+  for (const effect of effects) {
+    triggerEffect(effect)
+  }
+}
+
+export function triggerEffect(effect: ReactiveEffect) {
+  effect.run()
 }
