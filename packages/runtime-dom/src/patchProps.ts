bc4db73f57719d8effec618d6728295cc1604b75
jinrongda <2368741842@qq.com>
2023-03-19 15:44:10 +0800

    feat(main): [runtime-core][render函数]->区分处理ELEMENT节点各种属性的挂载

--- packages/runtime-dom/src/patchProps.ts
+++ packages/runtime-dom/src/patchProps.ts
@@ -1,10 +1,35 @@
 import { isOn } from '@vue/shared'
+import { patchAttr } from './modules/attrs'
 import { patchClass } from './modules/class'
+import { patchDOMProp } from './modules/props'
 
 export const patchProp = (el: Element, key, prevValue, nextValue) => {
   if (key === 'class') {
     patchClass(el, nextValue)
   } else if (key === 'style') {
   } else if (isOn(key)) {
+  } else if (shouldSetAsProp(el, key)) {
+    patchDOMProp(el, key, nextValue)
+  } else {
+    patchAttr(el, key, nextValue)
   }
+}
+
+function shouldSetAsProp(el: Element, key: string) {
+  // form属性只读
+  if (key === 'form') {
+    return false
+  }
+
+  // input的属性必须使用setAttribute设置
+  if (key === 'list' && el.tagName === 'INPUT') {
+    return false
+  }
+
+  // TEXTAREA设置属性也必须使用setAttribute
+  if (key === 'type' && el.tagName === 'TEXTAREA') {
+    return false
+  }
+
+  return key in el
 }
