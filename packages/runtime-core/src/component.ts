db481bb3781b3781f2258e1dabb4c124045edbe4
jinrongda <2368741842@qq.com>
2023-04-11 16:08:01 +0800

    feat: 通过修改生命周期函数的this指向完成对data中数据的访问

--- packages/runtime-core/src/component.ts
+++ packages/runtime-core/src/component.ts
@@ -44,10 +44,10 @@ function setupStatefulComponent(instance) {
 
 export function finishComponentSetup(instance) {
   const Component = instance.type
-	
+
   // 从type中取出render挂载到instance上
   instance.render = Component.render
-	
+
   // applyOption方法中挂载其它vue常用配置到instance身上
   applyOptions(instance)
 }
@@ -61,12 +61,12 @@ function applyOptions(instance: any) {
     beforeMount,
     mounted
   } = instance.type
-	
+
   // 若存在beforeCreate生命周期，执行
   if (beforeCreate) {
-    callHook(beforeCreate)
+    callHook(beforeCreate, instance.data)
   }
-	
+
   // 存在data函数（一般return一个对象），我们拿到data返回的对象变成reactive响应式对象后挂载到instance身上
   if (dataOptions) {
     const data = dataOptions()
@@ -74,21 +74,21 @@ function applyOptions(instance: any) {
       instance.data = reactive(data)
     }
   }
-	
+
   // 若存在created生命周期，执行
   if (created) {
-    callHook(created)
+    callHook(created, instance.data)
   }
 
   function registerLifecycleHook(register: Function, hook?: Function) {
-    register(hook, instance)
+    register(hook?.bind(instance.data), instance)
   }
-	
+
   // registerLifecycleHook函数的作用就是注册其它的那些生命周期，说白了就是把beforeCreate和created之外的那些生命周期挂载到instance身上，具体实现细节就不看了
   registerLifecycleHook(onBeforeMount, beforeMount)
   registerLifecycleHook(onMounted, mounted)
 }
 
-function callHook(hook: Function) {
-  hook()
+function callHook(hook: Function, proxy) {
+  hook.bind(proxy)()
 }
